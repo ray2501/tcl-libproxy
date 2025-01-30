20e8e1b638abb4e90dc924bbef4a673310ac2a97
Danilo Chang <ray2501@gmail.com>
2025-01-30 20:11:37 +0800

    Bump version to v0.3

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded libproxy 0.2 \
-	    [list load [file join $dir libtcl9libproxy0.2.so] [string totitle libproxy]]
+    package ifneeded libproxy 0.3 \
+	    [list load [file join $dir libtcl9libproxy0.3.so] [string totitle libproxy]]
 } else {
-    package ifneeded libproxy 0.2 \
-	    [list load [file join $dir liblibproxy0.2.so] [string totitle libproxy]]
+    package ifneeded libproxy 0.3 \
+	    [list load [file join $dir liblibproxy0.3.so] [string totitle libproxy]]
 }
